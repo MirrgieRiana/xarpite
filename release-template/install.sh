1ecbeb865192e82343e115dce5d1a210ccc77c7f
_ <tacticsrealize2@gmail.com>
2025-12-09 09:54:14 +0900

    build: cleanup: remove: chmod

--- release-template/install.sh
+++ release-template/install.sh
@@ -32,7 +32,6 @@ echo "Updating /usr/local/bin/xa"
 destination=/usr/local/bin/xa
 rm -f "$destination" || exit
 ln -s "$SCRIPT_DIR"/xarpite "$destination" || exit
-chmod +x "$SCRIPT_DIR"/xarpite || exit
 echo "Updating /usr/local/bin/xarpite-update"
 destination=/usr/local/bin/xarpite-update
 rm -f "$destination" || exit
