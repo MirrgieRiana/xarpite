ef3bf4750642da4db479ba15ba17ae17cac7cb74
_ <tacticsrealize2@gmail.com>
2025-12-19 07:43:46 +0900

    Fix indentation for pages directory in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,7 +124,7 @@ val bundleRelease = tasks.register<Sync>("bundleRelease") {
     }
     from(tasks.named("jvmJar")) { into("bin/jvm") }
     from(project(":node").tasks.named("jsNodeProductionLibraryDistribution")) { into("bin/node") }
-    from("pages") { into("docs") }
+    from("pages")
     from(project(":playground").tasks.named("bundleRelease")) { into("playground") }
 }
 tasks.named("build").configure { dependsOn(bundleRelease) }
