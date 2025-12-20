9a0f96ed938643936587c151964186735896c74f
_ <tacticsrealize2@gmail.com>
2025-12-20 22:51:06 +0900

    Refactor module cache and file system usage in ModuleMounts
    Simplified the creation of the module cache by using Path instead of okio.Path. Improved error messages and inlined file system retrieval for reading module files. This refactor enhances code clarity and error handling.

--- src/commonMain/kotlin/mirrg/xarpite/cli/ModuleMounts.kt
+++ src/commonMain/kotlin/mirrg/xarpite/cli/ModuleMounts.kt
@@ -13,19 +13,17 @@ import okio.Path.Companion.toPath
 fun createModuleMounts(filePath: String, mountsFactory: (String) -> List<Map<String, FluoriteValue>>): List<Map<String, FluoriteValue>> {
     return mapOf(
         "USE" to run {
-            val moduleCache = mutableMapOf<okio.Path, FluoriteValue>()
+            val moduleCache = mutableMapOf<Path, FluoriteValue>()
             val baseDir by lazy {
-                val parent = filePath.toPath().parent
-                parent?.normalized() ?: throw FluoriteException("Parent directory is required".toFluoriteString())
+                filePath.toPath().parent?.normalized() ?: throw FluoriteException("Cannot determine base directory.".toFluoriteString())
             }
             FluoriteFunction { arguments ->
                 if (arguments.size != 1) usage("USE(file: STRING): VALUE")
                 val file = arguments[0].toFluoriteString().value
-                if (!file.startsWith("./")) throw FluoriteException("Path must start with \"./\"".toFluoriteString())
-                val fileSystem = getFileSystem().getOrThrow()
+                if (!file.startsWith("./")) throw FluoriteException("""Path must start with "./".""".toFluoriteString())
                 val modulePath = baseDir.resolve(file.drop(2).toPath()).normalized()
                 moduleCache.getOrPut(modulePath) {
-                    val src = fileSystem.read(modulePath) { readUtf8() }
+                    val src = getFileSystem().getOrThrow().read(modulePath) { readUtf8() }
                     val evaluator = Evaluator()
                     evaluator.defineMounts(mountsFactory(modulePath.toString()))
                     evaluator.get(src)
