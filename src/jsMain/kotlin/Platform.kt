7b0b330b8ebafc124736bfac5ab227e365643416
_ <tacticsrealize2@gmail.com>
2026-01-12 19:31:50 +0900

    Simplify writeBytesToStderr implementation

--- src/jsMain/kotlin/Platform.kt
+++ src/jsMain/kotlin/Platform.kt
@@ -28,14 +28,7 @@ var writeBytesToStdoutImpl: (suspend (ByteArray) -> Unit)? = null
 actual suspend fun writeBytesToStdout(bytes: ByteArray) = writeBytesToStdoutImpl!!(bytes)
 
 var writeBytesToStderrImpl: (suspend (ByteArray) -> Unit)? = null
-actual suspend fun writeBytesToStderr(bytes: ByteArray) {
-    val impl = writeBytesToStderrImpl
-    if (impl != null) {
-        impl(bytes)
-    } else {
-        // テスト環境では何もしない
-    }
-}
+actual suspend fun writeBytesToStderr(bytes: ByteArray) = writeBytesToStderrImpl!!(bytes)
 
 actual suspend fun executeProcess(process: String, args: List<String>): String {
     throw WorkInProgressError("EXEC is an experimental feature and is currently only available on JVM and Native platforms")
