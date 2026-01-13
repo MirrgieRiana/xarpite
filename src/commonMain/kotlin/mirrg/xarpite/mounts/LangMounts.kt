e1692f3b4d82cfd647014e3f6bd9d1fd2d532f5c
_ <tacticsrealize2@gmail.com>
2026-01-13 20:59:20 +0900

    Refactor createLangMounts to use mutableMap
    Replaces the mapOf construction with a mutableMap for mounting language values and functions. This change improves readability and maintainability by allowing stepwise addition of entries and easier future modifications.

--- src/commonMain/kotlin/mirrg/xarpite/mounts/LangMounts.kt
+++ src/commonMain/kotlin/mirrg/xarpite/mounts/LangMounts.kt
@@ -14,72 +14,73 @@ import mirrg.xarpite.compilers.objects.FluoriteStream
 import mirrg.xarpite.compilers.objects.FluoriteValue
 import mirrg.xarpite.compilers.objects.cache
 import mirrg.xarpite.compilers.objects.collect
-import mirrg.xarpite.compilers.objects.consume
 import mirrg.xarpite.compilers.objects.invoke
 
 fun createLangMounts(coroutineScope: CoroutineScope, out: suspend (FluoriteValue) -> Unit): List<Map<String, FluoriteValue>> {
-    return mapOf(
-        "NULL" to FluoriteNull,
-        "N" to FluoriteNull,
-        "TRUE" to FluoriteBoolean.TRUE,
-        "T" to FluoriteBoolean.TRUE,
-        "FALSE" to FluoriteBoolean.FALSE,
-        "F" to FluoriteBoolean.FALSE,
-        "EMPTY" to FluoriteStream.EMPTY,
-        "E" to FluoriteStream.EMPTY,
-        "LOOP" to FluoriteStream {
-            while (true) {
-                emit(FluoriteNull)
-            }
-        },
-        "SLEEP" to FluoriteFunction { arguments ->
-            when (arguments.size) {
-                0 -> yield()
+    val mounts = mutableMapOf<String, FluoriteValue>()
 
-                1 -> {
-                    val time = arguments[0] as FluoriteNumber
-                    val millis = time.toInt().toLong()
-                    if (millis == 0L) {
-                        yield()
-                    } else {
-                        delay(millis)
-                    }
-                }
+    mounts["NULL"] = FluoriteNull
+    mounts["N"] = FluoriteNull
+    mounts["TRUE"] = FluoriteBoolean.TRUE
+    mounts["T"] = FluoriteBoolean.TRUE
+    mounts["FALSE"] = FluoriteBoolean.FALSE
+    mounts["F"] = FluoriteBoolean.FALSE
+    mounts["EMPTY"] = FluoriteStream.EMPTY
+    mounts["E"] = FluoriteStream.EMPTY
+    mounts["LOOP"] = FluoriteStream {
+        while (true) {
+            emit(FluoriteNull)
+        }
+    }
+    mounts["SLEEP"] = FluoriteFunction { arguments ->
+        when (arguments.size) {
+            0 -> yield()
 
-                else -> usage("SLEEP([milliseconds: NUMBER]): NULL")
-            }
-            FluoriteNull
-        },
-        "CALL" to FluoriteFunction { arguments ->
-            if (arguments.size != 2) usage("CALL(function: FUNCTION; arguments: ARRAY<VALUE>): VALUE")
-            val function = arguments[0]
-            val argumentsArray = arguments[1] as FluoriteArray
-            function.invoke(argumentsArray.values.toTypedArray())
-        },
-        "LAUNCH" to FluoriteFunction { arguments ->
-            if (arguments.size != 1) usage("<T> LAUNCH(function: () -> T): PROMISE<T>")
-            val function = arguments[0]
-            val promise = FluoritePromise()
-            coroutineScope.launch {
-                try {
-                    promise.deferred.complete(function.invoke(emptyArray()).cache())
-                } catch (e: Throwable) {
-                    promise.deferred.completeExceptionally(e)
-                }
-            }
-            promise
-        },
-        "OUT" to FluoriteFunction { arguments ->
-            arguments.forEach {
-                if (it is FluoriteStream) {
-                    it.collect { item ->
-                        out(item)
-                    }
+            1 -> {
+                val time = arguments[0] as FluoriteNumber
+                val millis = time.toInt().toLong()
+                if (millis == 0L) {
+                    yield()
                 } else {
-                    out(it)
+                    delay(millis)
                 }
             }
-            FluoriteNull
-        },
-    ).let { listOf(it) }
+
+            else -> usage("SLEEP([milliseconds: NUMBER]): NULL")
+        }
+        FluoriteNull
+    }
+    mounts["CALL"] = FluoriteFunction { arguments ->
+        if (arguments.size != 2) usage("CALL(function: FUNCTION; arguments: ARRAY<VALUE>): VALUE")
+        val function = arguments[0]
+        val argumentsArray = arguments[1] as FluoriteArray
+        function.invoke(argumentsArray.values.toTypedArray())
+    }
+    mounts["LAUNCH"] = FluoriteFunction { arguments ->
+        if (arguments.size != 1) usage("<T> LAUNCH(function: () -> T): PROMISE<T>")
+        val function = arguments[0]
+        val promise = FluoritePromise()
+        coroutineScope.launch {
+            try {
+                promise.deferred.complete(function.invoke(emptyArray()).cache())
+            } catch (e: Throwable) {
+                promise.deferred.completeExceptionally(e)
+            }
+        }
+        promise
+    }
+    mounts["OUT"] = FluoriteFunction { arguments ->
+        arguments.forEach {
+            if (it is FluoriteStream) {
+                it.collect { item ->
+                    out(item)
+                }
+            } else {
+                out(it)
+            }
+        }
+        FluoriteNull
+    }
+
+    return listOf(mounts)
 }
